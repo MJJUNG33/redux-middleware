407ad99bc24fa1976de124d67e226473c9d86fc9
Minji Jung <jung2674@gmail.com>
2024-03-29 02:27:54 +1300

    Style tablet and desktop size screen

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -21,49 +21,60 @@ const ProductDetailPage = () => {
   return (
     <Container
       fluid
-      className="d-flex flex-column align-items-center justify-content-center mb-2"
+      className="d-flex flex-column align-items-center justify-content-center mb-2 product-detail "
     >
-      <Col xs={12}>
-        <Image src={product.img} alt={product.title} fluid />
-      </Col>
-      <Col xs={12} className="d-flex flex-column align-items-start mt-2 ">
-        <Row className="w-100">
-          <Col xs={4} className="pe-0">
-            {product.choice === true ? (
-              <p className="extra-info choice">Conscious choice</p>
-            ) : (
-              <div className="choice"></div>
-            )}
-          </Col>
-          <Col xs={5}>
-            {product?.new === true ? (
-              <p className="extra-info new-arrival">New Arrival</p>
-            ) : (
-              <div className="new-arrival"></div>
-            )}{" "}
-          </Col>
-        </Row>
+      <Row className="w-100">
+        <Col xs={12} md={6}>
+          <Image src={product.img} alt={product.title} fluid />
+        </Col>
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex flex-column align-items-start mt-2"
+        >
+          <Row className="w-100 ">
+            <Col xs={5} md={12} lassName="pe-0">
+              {product.choice === true ? (
+                <p className="extra-info choice detail-choice-new">
+                  Conscious choice
+                </p>
+              ) : (
+                <div className="choice"></div>
+              )}
+            </Col>
+            <Col xs={5} className="ps-0 detail-new">
+              {product?.new === true ? (
+                <p className="extra-info new-arrival detail-choice-new">
+                  New Arrival
+                </p>
+              ) : (
+                <div className="new-arrival"></div>
+              )}{" "}
+            </Col>
+          </Row>
 
-        <Col xs={12} className="fw-bold">
-          <p className="mb-0">{product.title}</p>
-          <p>${product?.price}</p>
+          <Col xs={12} className="fw-bold detail-title-price mb-2">
+            <p className="detail-title-price">{product.title}</p>
+            <p>${product?.price}</p>
+          </Col>
+          <Col xs={12} className="mb-3">
+            <Form.Select className="w-100" aria-label="size select">
+              <option>Available size</option>
+              {product.size &&
+                product.size.map((size) => (
+                  <option key={size} value={size}>
+                    {size}
+                  </option>
+                ))}
+            </Form.Select>
+          </Col>
+          <Col className="w-100 mt-3 mb-2">
+            <Button className="w-100 p-2 add-btn fw-bold" variant="danger">
+              Add to cart
+            </Button>
+          </Col>
         </Col>
-        <Col xs={12}>
-          <Form.Select className="w-50" aria-label="size select">
-            {product.size &&
-              product.size.map((size) => (
-                <option key={size} value={size}>
-                  {size}
-                </option>
-              ))}
-          </Form.Select>
-        </Col>
-        <Col className="w-100 mt-3 mb-2">
-          <Button className="w-100 p-2" variant="danger">
-            Add to cart
-          </Button>
-        </Col>
-      </Col>
+      </Row>
     </Container>
   );
 };
