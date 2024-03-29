32ae67d9b02f3a45a23680a26b61c665d0786425
Minji Jung <jung2674@gmail.com>
2024-03-30 00:31:06 +1300

    Styling message for not matching with search input

--- src/page/ProductsPage.js
+++ src/page/ProductsPage.js
@@ -1,51 +1,3 @@
-// import React, { useEffect, useMemo, useState } from "react";
-// import ProductsCard from "../component/ProductsCard";
-// import { Container, Col, Row } from "react-bootstrap";
-// import { useSearchParams } from "react-router-dom";
-
-// const ProductsPage = () => {
-//   const [products, setProducts] = useState([]);
-//   const [query, setQuery] = useSearchParams();
-
-//   const getProducts = async () => {
-//     const searchQuery = query.get("q") || "";
-//     console.log("what os qeury?", searchQuery);
-//     // const url = `https://my-json-server.typicode.com/MJJUNG33/shopping-mall/products?q=${searchQuery}`;
-//     const url = `http://localhost:5000/products?q=${searchQuery}`;
-
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
-//     setProducts(data);
-//   };
-
-//   const serachProduct = useMemo(() => {
-//     return products.filter((product) => {
-//       product.title.toLowerCase().include(query.get("q").toLowerCase());
-//     });
-//   }, [products, query]);
-
-//   useEffect(() => {
-//     getProducts();
-//   }, [query]);
-
-//   return (
-//     <Container className="mt-5 mb-5">
-//       <Row xs={1} md={2} lg={4} className="align-items-start m-1 product-list">
-//         {products.map((product, i) => {
-//           return (
-//             <Col key={i}>
-//               <ProductsCard product={product} />
-//             </Col>
-//           );
-//         })}
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default ProductsPage;
-
 import React, { useEffect, useState, useMemo } from "react";
 import ProductsCard from "../component/ProductsCard";
 import { Container, Col, Row } from "react-bootstrap";
@@ -87,8 +39,9 @@ const ProductsPage = () => {
   return (
     <Container className="mt-5 mb-5">
       {filteredProducts.length === 0 ? (
-        <div className="text-center">
-          Sorry, there are no products for "{query.get("q")}"
+        <div className="not-find-product">
+          Sorry, there is no matching items with{" "}
+          <p className="search-input-value">"{query.get("q")}"</p>
         </div>
       ) : (
         <Row
